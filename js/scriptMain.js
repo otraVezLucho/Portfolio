54823f37f677cbc0e79a181ceefbe4735ce715ee
luisvillada <otrodeluisvillada@gmail.com>
2025-12-12 23:07:42 -0500

    aun estoy agregando los proyectos actuales, y dandole estilo a los elementos

--- js/scriptMain.js
+++ js/scriptMain.js
@@ -1,5 +1,13 @@
 import {addTask,deleteTask} from "../js/toDoscript.js";
 
+
+document.addEventListener('DOMContentLoaded', ()=> {
+alert("✨ This experience is under wizard creation ✨\n\nFor the best magic, please open this on a desktop or laptop!")
+});
+
+
+
+//From here this code works to Task List
 const inputText = document.getElementById("textTaskId");
 const buttonTask = document.getElementById("addTask");
 const buttonDelete = document.getElementById("deleteTask");
