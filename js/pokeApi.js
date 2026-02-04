a9b19f713df6a7617d860d4c04cc00fd08bf915b
otraVezLucho <otrodeluisvillada@gmail.com>
2026-02-04 00:51:01 -0500

    se implementa lista de pokemones, tengo que arreglar porque no me esta guardando los nombres de los  siguientes 30pokemon

--- js/pokeApi.js
+++ js/pokeApi.js
@@ -1,21 +1,20 @@
 const pokemonList = [];
 let counter = 0;
 let counter2 = 0;
-export async function getPokemonList() {
-    counter+=30;
+let iteradorBucles = 0;export async function getPokemonList() {
+    counter++;
     console.log(counter);
 
     const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=40');
+    
 
     if ( response === 404){
         alert("Unable to fetch the pokemon list")
-    }else{
-
+    }else if(counter === 1){
         //console.log(response);
         console.log();
         console.log();
         const data = await response.json();
-       
         for(let i = 0; i < data.results.length; i++){
             counter2 = i+1;
             pokemonList.push(data.results[i].name);
@@ -26,6 +25,20 @@ export async function getPokemonList() {
             
         }
         
+    }else if(counter>1){
+        const data = await response.json();
+        const nextUrl = data.next;
+        const nextResponse = await fetch(nextUrl);
+        const nextData = await nextResponse.json();
+
+        console.log(nextData.results);
+        for (let i = 30; i < nextData.results.length; i++) {
+            counter2 = i + 1;
+            pokemonList.push(nextData.results[i].name);
+            console.log(pokemonList[i]);
+
+            createCard(pokemonList[i], await searchPokemon(nextData.results[i].name))
+        }
     }
 }
 export async  function  searchPokemon(name) {
