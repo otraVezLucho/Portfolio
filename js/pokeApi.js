0798793d4fa88eac7f5db7bd44342befb4b9da2c
otraVezLucho <otrodeluisvillada@gmail.com>
2026-02-06 10:31:28 -0500

    Se completa seccion de API pokemones

--- js/pokeApi.js
+++ js/pokeApi.js
@@ -83,21 +83,22 @@ export function createCard(name, img){
     favoriteButton.addEventListener('click', addToFavorite);
 
     
-    
-
     const deleteButton = document.createElement('i');
+    deleteButton.style.display= 'none';
     deleteButton.classList.add('bx');
     deleteButton.classList.add('bxs-trash-alt');
     deleteButton.id = 'deleteButton';
+    
 
-    containerButtonsCard.append(favoriteButton);
+    
+
+    containerButtonsCard.append(favoriteButton,deleteButton);
 
     //Adding all components to the card
     pokeCard.append(titleCard, pokeImg, containerButtonsCard);
 
     return pokeCard;
 }
-
 const unitSearchContentSection = document.querySelector('.unitSearchContentNone');
 
 export async function searchUnitPokemon(name){
@@ -121,59 +122,52 @@ export async function searchUnitPokemon(name){
 }
 
 export function cleanSections(){
+    //The search one pokemon will be cleaned 
     unitSearchContentSection.innerHTML = "";
+    //The list of pokecards will be cleaned as well
     getListContent.innerHTML = "";
     //Tengo que reiniciar la URL 
     nextUrl = 'https://pokeapi.co/api/v2/pokemon?limit=40';
     getListButton.textContent = "Check all pokemon";
 }
-export function createCardFavorite(name, img) {
 
-
-    //Card 
-    const pokeCard = document.createElement('div');
-    pokeCard.classList.add('pokeCard');
-    pokeCard.id = 'pokeCardId' + counter2;
-
-    //TITLE CARD
-    const titleCard = document.createElement('p');
-    titleCard.classList.add('titleCard')
-    titleCard.innerText = name;
-
-    //Image
-    const pokeImg = document.createElement('img');
-    pokeImg.id = "imgCard";
-    pokeImg.src = img;
-
-
-    //container
-    const containerButtonsCard = document.createElement('div');
-    containerButtonsCard.classList.add('cardButtonSection');
-
-    //card buttons
-    const deleteButton = document.createElement('i');
-    deleteButton.classList.add('bx');
-    deleteButton.classList.add('bxs-trash-alt');
-    deleteButton.id = 'deleteButton';
-
-    containerButtonsCard.append(deleteButton);
-
-    //Adding all components to the card
-    pokeCard.append(titleCard, pokeImg, containerButtonsCard);
-
-    return pokeCard;
-}
 
 const favoriteContainer = document.querySelector('.favoritesSectionNone');
+const favoriteCardContainer = document.querySelector('.favoriteCardContainer');
+let totalCards = favoriteCardContainer.querySelectorAll('.pokeCard');
+console.log("total pokemon cards in the favorite container: ", totalCards.length );
+
+
 export function addToFavorite(e){
-
+    favoriteContainer.classList.remove('favoritesSectionNone');
     favoriteContainer.classList.add('favoritesSection');
-
     let cardToSend = e.target.parentNode.parentNode;
 
-    favoriteContainer.append(cardToSend);
+    //Show delete button
+    cardToSend.querySelector('.bxs-trash-alt').style.display='inline-block';
+    //Hide star button
+    cardToSend.querySelector('.bxs-star').style.display = 'none';
+    // Delete button can be only showed in favorites section
+    
+    const deleteButton = cardToSend.querySelector('.bxs-trash-alt'); //document.getElementById('deleteButton');
+    
+   
+    const containerButtonsCard = cardToSend.querySelector('.cardButtonSection');
+    totalCards = favoriteCardContainer.querySelectorAll('.pokeCard');
+    console.log("total pokemon cards in the favorite container: ", totalCards.length+1);
+    
+    deleteButton.addEventListener('click', deleteCard);
+    favoriteCardContainer.append(cardToSend);
     console.log("Se Ejecuto todo");
     
+}
 
 
+function deleteCard (e){
+    let cardToDelete = e.target.parentNode.parentNode;
+    cardToDelete.remove();
+    if (favoriteCardContainer.querySelectorAll('.pokeCard').length === 0) {
+        favoriteContainer.classList.remove('favoritesSection');
+        favoriteContainer.classList.add('favoritesSectionNone');
+    }
 }
